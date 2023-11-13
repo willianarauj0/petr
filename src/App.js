7b51ec84f81e833379115c96e618e77a8f7ab8bb
will <willianeves66@gmail.com>
2023-11-13 16:20:46 -0300

    upp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import './App.css'; 
+import Cabecalho from './componentes/header/header';
 
 function App() {
   return (
@@ -9,39 +10,7 @@ function App() {
             <h1>PetGo</h1>
           </div>
           <div className="col-10">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
-              <div className="collapse navbar-collapse alinhamento" id="navbarNav">
-                <ul className="navbar-nav ml-auto">
-                  <li className="nav-item active">
-                    <a className="nav-link" href="#">
-                      Home
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      Link
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      Another Link
-                    </a>
-                  </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                  <input
-                    className="form-control mr-sm-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                  />
-                  <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-                    Search
-                  </button>
-                </form>
-              </div>
-            </nav>
+            <Cabecalho />
           </div>
         </div>
       </div>
