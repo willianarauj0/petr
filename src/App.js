f193b424d13f0d6447be557e53a2f1f861b3ac10
willianarauj0 araujo <willianeves66@gmail.com>
2023-11-13 19:01:54 +0000

    up

--- src/App.js
+++ src/App.js
@@ -10,18 +10,8 @@ function App() {
           </div>
           <div className="col-10">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNav">
+
+              <div className="collapse navbar-collapse alinhamento" id="navbarNav">
                 <ul className="navbar-nav ml-auto">
                   <li className="nav-item active">
                     <a className="nav-link" href="#">
