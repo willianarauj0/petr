40bea11201d945b29cdb00c0ff02b36fd054243f
Jeferson Jesus <jgiovanimelo@gmail.com>
2023-11-27 16:00:17 -0300

    style: estilização do componente petshop

--- src/pages/Petshop.jsx
+++ src/pages/Petshop.jsx
@@ -3,19 +3,30 @@ import './Petshop.css'; // Importa o arquivo de estilos CSS
 
 const PetShop = () => {
   const [products] = useState([
-    { id: 1, name: 'Ração para cães', price: 30, imageUrl: 'link_para_imagem1.jpg' },
-    { id: 2, name: 'Brinquedo para gatos', price: 15, imageUrl: 'link_para_imagem2.jpg' },
-    { id: 3, name: 'Coleira para cães', price: 25, imageUrl: 'link_para_imagem3.jpg' },
+    { id: 1, name: 'Ração Gran Plus', price: 30, imageUrl:'link_para_imagem1.jpg'  },
+    { id: 2, name: 'Ração Pedigree', price: 30, imageUrl: 'link_para_imagem1.jpg' },
+    { id: 3, name: 'Ração PremieR', price: 30, imageUrl: 'link_para_imagem1.jpg' },
+    { id: 4, name: 'Coleira para cães', price: 25, imageUrl: 'link_para_imagem3.jpg' },
+    
+    { id: 5, name: 'Ração Friskies', price: 30, imageUrl: 'link_para_imagem1.jpg' },
+    { id: 6, name: 'Ração GoldeN', price: 30, imageUrl: 'link_para_imagem1.jpg' },
+    { id: 7, name: 'Ração Origens', price: 30, imageUrl: 'link_para_imagem1.jpg' },
+    { id: 8, name: 'Coleira Para Gatos', price: 25, imageUrl: 'link_para_imagem3.jpg' },
+    
+    { id: 2, name: 'Brinquedo Mordedor Nylon', price: 15, imageUrl: 'link_para_imagem2.jpg' },
+    { id: 3, name: 'Brinquedo Kroco ', price: 10, imageUrl: 'link_para_imagem3.jpg' },
+    
+
     // Adicione mais produtos conforme necessário
   ]);
 
   return (
     <div className="petshop-gallery">
-      <h1>Bem-vindo à nossa Loja Petshop!</h1>
+      <h1>Bem-vindo À Nossa Loja Pet Feliz!</h1>
       <div className="product-gallery">
         {products.map((product) => (
           <div key={product.id} className="product-card">
-            <img src={product.imageUrl} alt={product.name} className="product-image" />
+           
             <h3>{product.name}</h3>
             <p className="product-price">R$ {product.price.toFixed(2)}</p>
             <button className="add-to-cart-btn" onClick={() => alert(`Adicionado ao carrinho: ${product.name}`)}>
