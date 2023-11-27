11915011f7e84621321c4e47c15e3fd09127eaba
Jeferson Jesus <jgiovanimelo@gmail.com>
2023-11-27 13:24:26 -0300

    petshop atualizando

--- src/pages/Petshop.jsx
+++ src/pages/Petshop.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "./Petshop.css"
+import React, { useState } from 'react';
+import './Petshop.css'; // Importa o arquivo de estilos CSS
 
+const PetShop = () => {
+  const [products] = useState([
+    { id: 1, name: 'Ração para cães', price: 30, imageUrl: 'link_para_imagem1.jpg' },
+    { id: 2, name: 'Brinquedo para gatos', price: 15, imageUrl: 'link_para_imagem2.jpg' },
+    { id: 3, name: 'Coleira para cães', price: 25, imageUrl: 'link_para_imagem3.jpg' },
+    // Adicione mais produtos conforme necessário
+  ]);
 
-
-const Petshop = () => {
   return (
-    
-    <div className="container">
-      <div class="optional-box">
-        <h3>Petshop Doguinho feliz</h3>
+    <div className="petshop-gallery">
+      <h1>Bem-vindo à nossa Loja Petshop!</h1>
+      <div className="product-gallery">
+        {products.map((product) => (
+          <div key={product.id} className="product-card">
+            <img src={product.imageUrl} alt={product.name} className="product-image" />
+            <h3>{product.name}</h3>
+            <p className="product-price">R$ {product.price.toFixed(2)}</p>
+            <button className="add-to-cart-btn" onClick={() => alert(`Adicionado ao carrinho: ${product.name}`)}>
+              Adicionar ao Carrinho
+            </button>
+          </div>
+        ))}
       </div>
     </div>
-    )
-      
+  );
 };
 
-
-export default Petshop;
+export default PetShop;
